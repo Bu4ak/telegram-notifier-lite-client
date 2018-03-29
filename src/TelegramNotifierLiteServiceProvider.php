ee890cb16a75b4401d86d92ae64f34d817d713e5
bu4ak <igorbu4@gmail.com>
2018-03-29 22:13:39 +0700

    add ServiceProvider and config file

---
+++ src/TelegramNotifierLiteServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bu4ak\TelegramNotifierLite;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class TelegramNotifierLiteServiceProvider.
+ */
+class TelegramNotifierLiteServiceProvider extends ServiceProvider
+{
+    /**
+     * publish config.
+     */
+    public function boot()
+    {
+        $this->publishes(
+            [
+                __DIR__.'/config/notifier_lite.php' => config_path('notifier_lite.php'),
+            ], 'config'
+        );
+    }
+
+    /**
+     *
+     */
+    public function register()
+    {
+        $this->app->singleton(TelegramNotifierLite::class, function ($app) {
+            return new TelegramNotifierLite(config('notifier_lite.token.default'));
+        });
+    }
+}
